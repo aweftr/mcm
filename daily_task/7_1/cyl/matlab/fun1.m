f2233c91477a1293ad06cc814c2248e97c4c212f
aweftr <44022811+aweftr@users.noreply.github.com>
2019-07-01 20:39:05 +0800

    day 2 report

---
+++ daily_task/7_1/cyl/matlab/fun1.m
@@ -0,0 +1,8 @@
+function f = fun1(x)
+f=x(1)^2+x(2)^2+8;
+end
+
+
+% options = optimset;
+% [x,y]=fmincon('fun1',rand(2,1),[],[],[],[],zeros(2,1),[],...
+% 'fun2',options);
---
+++ daily_task/7_1/cyl/matlab/fun2.m
@@ -0,0 +1,4 @@
+function [g,h] = fun2(x)
+g = -x(1)^2 + x(2);
+h = -x(1) - x(2)^2 + 2;
+end
