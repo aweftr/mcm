de5a98bb36ebae8691ab47750d0d5c7b93013e8e
aweftr <44022811+aweftr@users.noreply.github.com>
2019-09-08 08:07:47 +0800

    references

---
+++ mock_exam/questions/A/3. BDSP Dataset/vol_time.m
@@ -0,0 +1,21 @@
+load B0005.mat
+cycle = B0005.cycle;
+size_of_cycle = size(cycle);
+hold on
+for i = 1:size_of_cycle(2)
+    if strcmp(cycle(i).type, 'charge')
+        charge_cycle = cycle(i);
+        if charge_cycle.data.Voltage_measured(1) >= 5
+            disp(i)
+        end
+        size_of_Time = size(charge_cycle.data.Time);
+        if size_of_Time(2) > 50
+            % plot(charge_cycle.data.Time(2:50), charge_cycle.data.Voltage_measured(2:50));
+            plot(charge_cycle.data.Time, charge_cycle.data.Voltage_measured);
+        else
+            plot(charge_cycle.data.Time, charge_cycle.data.Voltage_measured);
+        end
+    end
+end
+hold off
+    
